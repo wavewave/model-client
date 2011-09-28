0304cd68dfb44713af7221af2cded926ea2734a9
ianwookim <ianwookim@gmail.com>
2011-09-28 20:01:10 +0000

    make model-client compilable again.

--- lib/HEP/Automation/Model/Client/Job.hs
+++ lib/HEP/Automation/Model/Client/Job.hs
@@ -1,27 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module HEP.Automation.Model.Client.Job where
 
 import HEP.Automation.Model.Client.Config
 
-import Network.HTTP.Types hiding (statusCode)
+-- import Network.HTTP.Types hiding (statusCode)
 import Network.HTTP.Enumerator
 import qualified Data.ByteString.Lazy.Char8 as C
 import qualified Data.ByteString.Char8 as SC
 
 import Data.Aeson.Types
-import Data.Aeson.Encode
+-- import Data.Aeson.Encode
 import Data.Aeson.Parser
 
--- import Data.Attoparsec
-
-
-import Data.Data
+import qualified Data.Attoparsec as A
 
 import System.FilePath
 
 type Url = String 
 
 startCreate :: ModelConfiguration -> FilePath -> IO () 
-startCreate mc fp = do 
+startCreate _mc _fp = do 
   putStrLn "job started"
 
 startGet :: ModelConfiguration -> String -> IO () 
@@ -33,29 +32,29 @@ startGet mc name = do
 
 
 startPut :: ModelConfiguration -> FilePath -> IO () 
-startPut mc fp = do 
+startPut _mc _fp = do 
   putStrLn "job started"
 
 startDelete :: ModelConfiguration -> String -> IO () 
-startDelete mc name = do 
+startDelete _mc _name = do 
   putStrLn "job started"
 
 
-getJsonFromServer :: Url -> String -> IO (Maybe Value)
+getJsonFromServer :: Url -> String -> IO (Either String (Result Value))
 getJsonFromServer url api = do 
+  requestget <- parseUrl (url </> api)
   withManager $ \manager -> do
-    requestget <- parseUrl (url </> api)
     let requestgetjson = requestget { 
           requestHeaders = [ ("Accept", "application/json; charset=utf-8") ] 
         } 
     r <- httpLbs requestgetjson manager 
     if statusCode r == 200 
       then return . parseJson . SC.concat . C.toChunks . responseBody $ r
-      else return Nothing
+      else return (Left $ "status code : " ++ show (statusCode r)) 
 
-parseJson :: (Data a) => SC.ByteString -> Either String (Result a)
+parseJson :: (FromJSON a) => SC.ByteString -> Either String (Result a)
 parseJson bs =
-  let resultjson = parse json bs
+  let resultjson = A.parse json bs 
   in case resultjson of 
-       Success rjson -> Right (fromJSON rjson)
-       _            -> Left "error" 
+       (A.Done rest rjson) -> return (parse parseJSON rjson)
+       _                 -> Left "error" 
